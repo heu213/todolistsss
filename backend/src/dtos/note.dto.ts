e93f7ba6c90abb4a123e0bf16c805b8e904ac76f
heu213 <82399074+heu213@users.noreply.github.com>
2022-08-01 09:05:03 +0700

    Revert "Todolists"

--- backend/src/dtos/note.dto.ts
+++
@@ -1,6 +0,0 @@
-//DTO (đối tượng truyền dữ liệu) mô tả yêu cầu mong đợi sẽ trông như thế nào
-export class CreateNoteDTO {
-  name: string;
-  description: string;
-  createdAt: Date;
-}
--- backend/src/schemas/note.schema.ts
+++
@@ -1,7 +0,0 @@
-import * as mongoose from 'mongoose';
-const { Schema } = mongoose;
-
-export const NoteSchema = new Schema({
-  name: String,
-  description: String,
-});
